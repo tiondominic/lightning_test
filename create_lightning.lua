9a4007649611605c9c2024401744405cc2aff554
Tion Dominic <145018221+tiondominic@users.noreply.github.com>
2026-06-11 12:01:58 +0800

    First Commit

---
+++ create_lightning.lua
@@ -0,0 +1,68 @@
+local lightning = {}
+
+local random = math.random()
+
+function lightning:init( startx, starty, max_depth, max_jitter, min_jitter, max_width)
+    print("lightning created")
+    self.start_x = startx or 0
+    self.start_y = starty or 0
+    self.max_depth = 50
+    self.max_seg_length = 100
+    self.min_seg_length = 10
+    self.chonk = max_width or 10
+    self.min_jitter = min_jitter or 0
+    self.max_jitter = max_jitter or 180
+
+    self.segments = self:create(self.start_x, self.start_y, 0)
+
+end
+
+function lightning:create(x, y, depth)
+    local depth = depth
+
+    local frag = {}
+
+    for i = 0 + depth, self.max_depth do
+        local random_degree = love.math.random(self.min_jitter, self.max_jitter)
+        local angle_in_radians = math.rad(random_degree)
+        local current_length = love.math.random(self.min_seg_length, self.max_seg_length)
+                    
+        local x2 = x + math.cos(angle_in_radians) * current_length
+        local y2 = y + math.sin(angle_in_radians) * current_length
+        
+        if love.math.random(0, 10) <= 8 then
+            table.insert(frag, {type = "bolt", x=x,y=y,x2=x2,y2=y2})
+        else
+            local chain = lightning:create(x2, y2, depth+i)
+            table.insert(frag, {type = "chain", x=x, y=y, x2=x2, y2=y2, chain = chain})
+        end
+        x = x2
+        y = y2
+    end
+
+    return frag
+end
+
+function lightning:update(dt)
+
+end
+
+function lightning:recurse(chain, width)
+    if width <= 0 then width = 1 end
+    love.graphics.setLineWidth(width)
+    for _, i in ipairs(chain) do
+        if i.type == "bolt" then
+            love.graphics.line(i.x, i.y, i.x2, i.y2)
+        elseif i.type == "chain" then
+            love.graphics.line(i.x, i.y, i.x2, i.y2)
+            lightning:recurse(i.chain, width-2)
+        end
+    end
+end
+
+function lightning:draw()
+    lightning:recurse(self.segments, self.chonk)
+
+end
+
+return lightning
---
+++ main.lua
@@ -0,0 +1,25 @@
+local create_lightning = require "create_lightning"
+
+    S = create_lightning
+
+function love.load()
+    love.math.setRandomSeed(os.time())
+    width, height = love.graphics.getDimensions()
+    S:init(width/2, 0)
+end
+
+function love.update(dt)
+    -- S:update(dt)
+
+end
+
+function love.draw()
+    S:draw()
+end
+
+function love.keypressed(key)
+    if key == "g" then
+        S:init(width/2, 0)
+    end
+
+end
